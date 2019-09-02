f6eece74f4e9e4659f97b3c06286b11a428e977f
james@pensionbee.com <james@pensionbee.com>
2019-09-02 20:35:12 +0100

    test

--- server/app.js
+++ server/app.js
@@ -12,6 +12,11 @@ app.get('/api/products', (req, res) => {
   res.sendFile(path.join(__dirname, 'data', 'products.json'));
 });
 
+app.get('/', (req, res) => {
+  console.log('YOOOOO');
+});
+
 app.listen(port, () => {
   console.log(`[products] API listening on port ${port}.`);
+  console.log()
 });
