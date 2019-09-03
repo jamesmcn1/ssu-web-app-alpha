b52a8f1587073e0b1b459468295289e648ef4167
james@pensionbee.com <james@pensionbee.com>
2019-09-03 21:15:10 +0100

    logo in

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import './styles/Header.scss';
+import logo from '../assets/images/logo.png'
 
 export class Header extends Component {
   render() {
     return (
       <div className="header-container">
         <div className="header-div">
+          <img src={logo} />
           <h2>
             People
           </h2>
