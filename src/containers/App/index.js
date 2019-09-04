25a0550ee386a2ec996f643f592e0812110c855b
james@pensionbee.com <james@pensionbee.com>
2019-09-04 20:39:50 +0100

    routing fixed

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -9,16 +9,15 @@ import FloatCart from '../FloatCart';
 
 const App = () => (
   <React.Fragment>
-    <Router>
-      <main>
-        <Home />
-      </main>
+    <main>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/events" component={Events} />
+        <Route exact path="/people" component={People} />
+      </Router>
       <FloatCart />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/projects" component={Projects} />
-      <Route exact path="/events" component={Events} />
-      <Route exact path="/people" component={People} />
-    </Router>
+    </main>
   </React.Fragment>
 );
 
