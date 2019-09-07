c4fc1949cfb3c6890dbf970ce7118ce4c1d8bd94
james@pensionbee.com <james@pensionbee.com>
2019-09-07 20:19:08 +0100

    adds more homepage content

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -12,6 +12,27 @@ export class Home extends Component {
           src="https://res.cloudinary.com/nineteesvintage/image/upload/v1567537991/Sunny%20Side%20Up/american-public-power-association-419672-unsplash.jpg"
         >
           <Container>
+            <h1>Sunny Side Up</h1>
+          </Container>
+        </ImagePartial>
+        <ImagePartial
+          src="https://res.cloudinary.com/nineteesvintage/image/upload/v1567883250/Sunny%20Side%20Up/Project%20Images/rita-PP9m6__g1Ng-unsplash.jpg"
+        >
+          <Container>
+          </Container>
+        </ImagePartial>
+        <ImagePartial
+          src="https://res.cloudinary.com/nineteesvintage/image/upload/v1567883790/Sunny%20Side%20Up/Project%20Images/homepage_photo.jpg"
+        >
+          <Container>
+          </Container>
+        </ImagePartial>
+        <Container>
+        </Container>
+        <ImagePartial
+          src="https://res.cloudinary.com/nineteesvintage/image/upload/v1567883201/Sunny%20Side%20Up/Project%20Images/lyla.jpg"
+        >
+          <Container>
           </Container>
         </ImagePartial>
       </Screen>
