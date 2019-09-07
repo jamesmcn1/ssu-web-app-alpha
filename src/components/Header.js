b6b1bc5a55c879da3918856f96fbb6225697260a
james@pensionbee.com <james@pensionbee.com>
2019-09-07 19:07:05 +0100

    header fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,21 +10,19 @@ export class Header extends Component {
   render() {
     return (
       <div className="header-container">
-        <div className="not-mobile">
-          <div className="header-div not-mobile">
-            <Link className="link" to="/">
-              <img src={logo} />
-            </Link>
-            <Link className="link" to="/projects">
-              Projects
-            </Link>
-            <Link className="link" to="/events">
-              Events
-            </Link>
-            <Link className="link" to="/people">
-              People
-            </Link>
-          </div>
+        <div className="header-div not-mobile">
+          <Link className="link" to="/">
+            <img src={logo} />
+          </Link>
+          <Link className="link" to="/projects">
+            Projects
+          </Link>
+          <Link className="link" to="/events">
+            Events
+          </Link>
+          <Link className="link" to="/people">
+            People
+          </Link>
         </div>
       </div>
     );
