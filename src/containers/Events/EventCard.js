9960b0a26d2951e774e74e914e5067f2208d3821
James McNamara <james@pensionbee.com>
2019-09-29 13:19:14 +0100

    improves info content

--- src/containers/Events/EventCard.js
+++ src/containers/Events/EventCard.js
@@ -16,10 +16,14 @@ export class EventCard extends Component {
       <div className="event-card-container">
         <img src={src} className="img"/>
         <div class="info">
-          <h1>{name}</h1>
-          <h3>{date}</h3>
-          <h3>{venue}</h3>
-          <p>{description}</p>
+          <div className="info-content">
+            <h1>{name}</h1>
+            <div className="venue-and-date">
+              <h3>{date}</h3>
+              <h3>{venue}</h3>
+            </div>
+            <p>{description}</p>
+          </div>
           <div className="link"></div>
         </div>
       </div>
