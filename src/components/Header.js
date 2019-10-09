0fb179f7907cefb94d4e7bc1b11ff26292cab2e9
James McNamara <james@pensionbee.com>
2019-10-09 21:01:17 +0100

    mobile menu improvements

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,74 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import Home from "../containers/Home/index";
-import People from "../containers/People/index";
-
-import logo from '../assets/images/logo.png'
+import logo from '../assets/images/logo.png';
 
 export class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menuOpen: false,
+    };
+  }
+
+  openMenu = () => {
+    this.setState({ isOpen: true });
+  };
+  closeMenu = () => {
+    this.setState({ isOpen: false });
+  };
+
   render() {
+    let classes = ['header-container-mobile-only'];
+
+    if (!!this.state.isOpen) {
+      classes.push('open');
+    }
+
     return (
-      <div className="header-container">
-        <div className="header-div not-mobile">
-          <Link className="link" to="/">
+      <div>
+        <div className={classes.join(' ')}>
+          <span onClick={this.openMenu}>
             <img src={logo} />
-          </Link>
-          <Link className="link" to="/projects">
-            Projects
-          </Link>
-          <Link className="link" to="/events">
-            Events
-          </Link>
-          <Link className="link" to="/people">
-            People
-          </Link>
+          </span>
+          <div className="menu-div">
+            <div onClick={this.closeMenu}>
+              <img src={logo} />
+            </div>
+            <h2>
+              Sunny Side Up!
+            </h2>
+            <hr />
+            <Link className="link" to="/projects" onClick={this.closeMenu}>
+              <p>Projects</p>
+            </Link>
+            <hr />
+            <Link className="link" to="/events" onClick={this.closeMenu}>
+              <p>Events</p>
+            </Link>
+            <hr />
+            <Link className="link" to="/people" onClick={this.closeMenu}>
+              <p>People</p>
+            </Link>
+            <hr />
+          </div>
+        </div>
+        <div className="header-container-not-mobile">
+          <div className="header-div not-mobile">
+            <Link className="link" to="/">
+              <img src={logo} />
+            </Link>
+            <Link className="link" to="/projects">
+              Projects
+            </Link>
+            <Link className="link" to="/events">
+              Events
+            </Link>
+            <Link className="link" to="/people">
+              People
+            </Link>
+          </div>
         </div>
       </div>
     );
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Menu from '../Menu';
 
 import Home from '../Home';
 import Projects from '../Projects';
@@ -12,7 +11,6 @@ const App = () => (
   <React.Fragment>
     <main>
       <Router>
-        <Menu />
         <Route exact path="/" component={Home} />
         <Route exact path="/projects" component={Projects} />
         <Route exact path="/events" component={Events} />
