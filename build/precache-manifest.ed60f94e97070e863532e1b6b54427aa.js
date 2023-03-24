3126c33a68755685e48525111d11a4f9cf47b239
jamesmcn1 <james@pensionbee.com>
2023-03-24 11:36:02 +0000

    tries with build folder

---
+++ build/precache-manifest.ed60f94e97070e863532e1b6b54427aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff5b4ddf04b3af986c4c",
+    "url": "/static/css/main.785f4608.chunk.css"
+  },
+  {
+    "revision": "ff5b4ddf04b3af986c4c",
+    "url": "/static/js/main.c66b03e3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3ba2ab5f668010448d16",
+    "url": "/static/js/2.12602348.chunk.js"
+  },
+  {
+    "revision": "c2dc574b847baa136073c0ddc104c7f7",
+    "url": "/static/media/logo.c2dc574b.png"
+  },
+  {
+    "revision": "2aad15f6e878fceedf32839c5f1a1bce",
+    "url": "/index.html"
+  }
+];
